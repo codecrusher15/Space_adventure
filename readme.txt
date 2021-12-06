037a83d18b30057ff64f0d7afb9ec1bb9fb3e13d
codecrusher15 <pushkarpawar2002@gmail.com>
2021-12-06 11:04:52 +0530

    game added

---
+++ readme.txt
@@ -0,0 +1,11 @@
+A simple spaceship game made in python using pygame.
+
+lauch -> python3 space_adventure.py
+
+sounds from -> https://mixkit.co/
+
+Please do not use images directly.Use them from their sources and attribute the author.
+images sources 
+background.png from -> 'https://www.freepik.com/vectors/background' - Background vector created by freepik - www.freepik.com
+bg.jpeg from -> 'https://www.freepik.com/vectors/background' Background vector created by freepik - www.freepik.com
+player.png from -> Icons made by Freepik from href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com
---
+++ space_adventure.py
@@ -0,0 +1,154 @@
+import pygame
+import random
+from space_adventure_menu import menu
+from pygame import mixer
+SCORE_SPACE_ADVENTURE = 0
+lst = [];
+menu(lst);
+pygame.init()
+def game_start():
+    mixer.music.load('bgsound.wav')
+    mixer.music.play(-1)
+    start = True
+    screen = pygame.display.set_mode((600,800))
+    while start:
+        gs_img =pygame.image.load('startgame.png')
+        screen.blit(gs_img,(0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                start=False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    return space_adventure()
+                    start = False
+                if event.key == pygame.K_q:
+                    start = False
+        pygame.display.update()
+def space_adventure():
+
+    global SCORE_SPACE_ADVENTURE
+    SCORE_SPACE_ADVENTURE = 0
+    screen = pygame.display.set_mode((600,800))
+    WIDTH = 600
+    HEIGHT = 800
+    GAME_VEL = 5
+    running = True
+    class obj:
+        def __init__(self,x,y,img,xc,yc):
+            self.x = x
+            self.y = y
+            self.img = img
+            self.xc = xc
+            self.yc = yc
+            
+
+    #Icon and Title
+    pygame.display.set_caption("Space Adventure")
+    icon = pygame.image.load('player.png')
+    pygame.display.set_icon(icon)
+
+    #Player
+    player1 = obj(268,650,pygame.image.load('player.png'),0,0)
+
+    #PowerUP
+    power_up = obj(268,650,pygame.image.load('down.png'),0,0)
+
+    #Asteroid
+    asteroid_list = []
+    asteroid_list.append(obj(268,16,pygame.image.load('asteroid.png'),0,0))
+    asteroid_list.append(obj(60,16,pygame.image.load('asteroid.png'),0,0))
+    asteroid_list.append( obj(WIDTH-60,16,pygame.image.load('asteroid.png'),0,0))
+    asteroid_list.append(obj(164,16,pygame.image.load('asteroid.png'),0,0))
+
+    #Background Image
+    bg_img = pygame.image.load('background.png')
+
+    go_img = pygame.image.load('startgame.png')
+    #Print Score
+    score_font = pygame.font.SysFont(None,48)
+
+
+
+    def player(x,y):
+        screen.blit(player1.img,(x,y))
+
+    def player_movements():
+        player_pos_list = [60,164,268,WIDTH-224,WIDTH-124]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player1.xc = -10
+            if event.key == pygame.K_RIGHT:
+                player1.xc = 10
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player1.xc = 0
+        player1.x += player1.xc
+        #Restricting player movement boundaries
+        if player1.x<=0:
+            player1.x=0
+        elif player1.x>=WIDTH-64:
+            player1.x=WIDTH-64
+
+    def asteroid(asteroid,x,y):
+        screen.blit(asteroid.img,(x,y))
+
+    def asteroid_movements(asteroid,y_speed):
+        ast_pos_list = [60,164,268,WIDTH-224,WIDTH-124]
+        asteroid.yc = y_speed
+        asteroid.y+=asteroid.yc
+        if asteroid.y >= HEIGHT:
+            asteroid.y = -64
+            pos = random.randint(0,4)
+            asteroid.x = ast_pos_list[pos]
+            global SCORE_SPACE_ADVENTURE
+            SCORE_SPACE_ADVENTURE+=1
+
+    def powerUp():
+        global SCORE_SPACE_ADVENTURE
+        if(int(SCORE_SPACE_ADVENTURE%24)==0 and int(SCORE_SPACE_ADVENTURE)!=0):
+            screen.blit(power_up.img,(asteroid_list[0].x,power_up.y))
+            if(player1.x >= asteroid_list[0].x-64 and player1.x <= asteroid_list[0].x +64):
+                SCORE_SPACE_ADVENTURE+=4
+                mixer.Sound('powerup.wav').play()
+                return True
+
+    
+    def collision(asteroid,player):
+        if (asteroid.y >= 608 and asteroid.y <= 650) and (player1.x >= asteroid.x-60 and player1.x <= asteroid.x+60):
+            return True
+        return False
+    clock=pygame.time.Clock()
+    #Game Loop
+    while running:
+        clock.tick(70);
+        screen.blit(bg_img,(0,0))
+        score_img = score_font.render('Score:'+str(SCORE_SPACE_ADVENTURE),True,(255,255,255))
+        screen.blit(score_img,(20,20))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        player_movements()
+        player(player1.x,player1.y)
+        for asteroid_ele in asteroid_list:
+            asteroid_movements(asteroid_ele,GAME_VEL)
+            asteroid(asteroid_ele,asteroid_ele.x,asteroid_ele.y)
+        for asteroid_ele in asteroid_list:
+            if collision(asteroid_ele,player1):
+                    running = False
+                    GAME_VEL = 0
+                    explosion_sound = mixer.Sound('explosion.wav')
+                    explosion_sound.play()
+                    pygame. mixer. music. stop()
+                    pygame.time.delay(1500)
+                    pygame.display.update()
+                    lst.append(SCORE_SPACE_ADVENTURE)
+                    if(menu(lst)):
+                        game_start();
+        if(powerUp()):
+            GAME_VEL = GAME_VEL/2           
+        GAME_VEL+=SCORE_SPACE_ADVENTURE/5000
+        pygame.display.update()
+    return SCORE_SPACE_ADVENTURE
+if __name__=='__main__':
+    game_start()
